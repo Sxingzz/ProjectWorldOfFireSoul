54b4f9bc16244ce74f6db431c5aafd9476391d65
Sxingzz <saohuynh223@gmail.com>
2024-01-06 15:34:31 +0700

    no message

---
+++ Assets/MyGames/Scripts/GamePlay/CharacterLocomotion.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    private Animator animator;
+    private Vector2 userInput;
+    private bool isSprinting;
+    private bool isJumping;
+    private Rigidbody rb;
+
+
+    [SerializeField]
+    private float SprintingSpeed = 1.5f;
+    [SerializeField]
+    private float JumpForce = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        userInput.x = Input.GetAxis("Horizontal");
+        userInput.y = Input.GetAxis("Vertical");
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+        isJumping = Input.GetKeyDown(KeyCode.Space);
+
+        float speedMultiplier = isSprinting ? SprintingSpeed : 1.0f;
+
+        animator.SetFloat("InputX", userInput.x * speedMultiplier);
+        animator.SetFloat("InputY", userInput.y * speedMultiplier);
+
+        print($"User Input: {userInput}");
+
+        if (isSprinting)
+        {
+            jump();
+        }
+
+    }
+    void jump()
+    {
+        if ( rb != null )
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+}
